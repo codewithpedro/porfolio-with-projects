cca7f00be085b50a59e837fc25cf662415efa3a6
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-04-08 17:22:59 -0700

    removing old project files

--- digital-business-card/src/components/Portrait.js
+++
@@ -1,9 +0,0 @@
-import portrait from ".././images/self-portrait.jpg"
-
-function Portrait() {
-  return (
-    <img src={portrait} alt="profile picture" className='info--portrait'/>
-  )
-}
-
-export default Portrait;
