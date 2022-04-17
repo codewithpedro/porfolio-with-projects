56a50caae15c2209db054f4fef85fb0a6f74b626
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-04-17 15:38:03 -0700

    removed porfolio parentt directory

--- portfolio/src/components/ScrollToTop.js
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-import { useLayoutEffect } from "react";
-
-export default function ScrollToTop(){
-  const { pathname } = useLocation();
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-} 
--- portfolio/src/components/digital-business-card/Portrait.js
+++
@@ -1,7 +0,0 @@
-import portrait from "../../img/self-portrait.jpg"
-
-export default function Portrait() {
-  return (
-    <img src={portrait} alt="profile picture" className='portrait--img'/>
-  )
-}
